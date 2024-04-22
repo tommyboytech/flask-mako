a604e7a778f24e406580f429b6601d7c48b2b163
Ron M <643717+StreamOfRon@users.noreply.github.com>
2024-04-22 13:18:07 -0700

    Update setup.py with umt version tag

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Mako',
-    version='0.5',
+    version='0.5-umt',
     url='https://github.com/benselme/flask-mako',
     license='BSD',
     author='Beranger Enselme, Frank Murphy',
